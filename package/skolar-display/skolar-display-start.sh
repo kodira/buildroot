e0a0361fec200361f6c6b7c21a048df496004456
Markus Dittmann <dmarku@hrz.tu-chemnitz.de>
2014-11-13 15:31:57 +0100

    made keep-alive script exitable
    The startup script that kept the client application running in an
    infinite loop is now terminable under certain conditions. It monitors
    the exit code of the skolar-display client application. If the exit code
    has a certain value (42), it will not restart the application again.
    
    This is mostly a convenience function for testing and debugging
    purposes.

--- package/skolar-display/skolar-display-start.sh
+++ package/skolar-display/skolar-display-start.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
-while true; do skolar-display; done
+BINARY=skolar-display
+# the return code that should terminate the execution of the display application
+TERMINATE_EXIT_CODE=42
+while true;
+do
+	${BINARY}
+	RETURN=$?
+	echo ${RETURN}
+	if [ ${RETURN} -eq ${TERMINATE_EXIT_CODE} ];
+	then
+		break
+	fi
+done
 
